866a15a2044f77ee787e1b18a2da133cd54f5c19
Gezer <guydunski@gmail.com>
2015-12-24 18:33:02 +0200

    change

--- RummikubGUI/src/Scenes/GameSceneController.java
+++ RummikubGUI/src/Scenes/GameSceneController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Scenes;
+package scenes;
 
 import ScreensControlFrameWork.ControlledScreen;
 import ScreensControlFrameWork.ScreensController;
--- RummikubGUI/src/Scenes/MainMenuSceneController.java
+++ RummikubGUI/src/Scenes/MainMenuSceneController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Scenes;
+package scenes;
 
 import ScreensControlFrameWork.ControlledScreen;
 import ScreensControlFrameWork.ScreensController;
@@ -37,7 +37,6 @@ public class MainMenuSceneController implements Initializable, ControlledScreen 
     @Override
     public void setScreenParent(ScreensController screenPage) {
         myController = screenPage;
-        
     }
     
     @FXML
